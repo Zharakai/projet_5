052050cd8ed8f4f979b53e22b479ebe826822fa5
Yannis Almanric <yannis.almanric@gmail.com>
2020-05-05 18:16:14 +0200

    Intégrer les données JS de manière dynamique dans le HTML (produits), en cours...

--- application.js
+++ application.js
@@ -1,6 +1,5 @@
 const productsDOM = document.querySelector('.container-products');
 
-
 // Getting the products
 class Products {
     async getProducts() {
@@ -9,14 +8,42 @@ class Products {
             let data = await result.json();
             return data;
         } catch (error) {
-            Console.log(error)
-        }
-        
+            Console.log(error);
+        }  
     }
 }
 
 class UserInterface {
-
+    displayProducts(products) {
+        let result = '';
+        products.forEach(product => {
+            result +=`
+            <section class="container-products">
+                <article>
+                    <div class=""><a href="produit.html"><img src=${product.imageUrl} alt="Ours en peluche Norbert"></a></div>
+                    <h3>${product.name} - ${product.price} €</h3>
+                </article>
+                <article>
+                    <div class=""><a href="produit.html"><img src="./images/teddy_2.jpg" alt="Ours en peluche Arnold"></a></div>
+                    <h3>Arnold - 39 €</h3>
+                </article>
+                <article>
+                    <div class=""><a href="produit.html"><img src="./images/teddy_3.jpg" alt="Ours en peluches Lenny et Carl"></a></div>
+                    <h3>Lenny et Carl - 59 €</h3>
+                </article>
+                <article>
+                    <div class=""><a href="produit.html"><img src="./images/teddy_4.jpg" alt="Ours en peluche Gustav"></a></div>
+                    <h3>Gustav - 45 €</h3>
+                </article>
+                <article>
+                    <div class=""><a href="produit.html"><img src="./images/teddy_5.jpg" alt="Ours en peluche Garfunkel"></a></div>
+                    <h3>Garfunkel - 55 €</h3>
+                </article>
+            </section>
+            `;
+        });
+        productsDOM.innerHTML = result;
+    }
 }
 
 class Storage {
@@ -24,6 +51,9 @@ class Storage {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UserInterface();
     const products = new Products();
-    products.getProducts().then(data => console.log(data));
+
+    // Get all products
+    products.getProducts().then(products => console.log(products));
 });
