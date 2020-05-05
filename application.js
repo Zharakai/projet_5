611c097a491f53a862ce79979b062492e82965b5
Yannis Almanric <yannis.almanric@gmail.com>
2020-05-05 15:03:54 +0200

    Récupérer les produits de l'API avec fetch

--- application.js
+++ application.js
@@ -1,1 +1,29 @@
-console.log(Test)
+const productsDOM = document.querySelector('.container-products');
+
+
+// Getting the products
+class Products {
+    async getProducts() {
+        try {
+            let result = await fetch('http://localhost:3000/api/teddies');
+            let data = await result.json();
+            return data;
+        } catch (error) {
+            Console.log(error)
+        }
+        
+    }
+}
+
+class UserInterface {
+
+}
+
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const products = new Products();
+    products.getProducts().then(data => console.log(data));
+});
